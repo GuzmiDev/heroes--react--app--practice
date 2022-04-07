1cd321b3fddc65765b5e7f41263e4449144b56a7
Erick Guzman <guzmi.dev@gmail.com>
2022-04-06 22:42:41 -0600

    :art: logout and login without AUTH

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import HeroesApp from "./HeroesApp";
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<HeroesApp />);
+root.render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>
+);
