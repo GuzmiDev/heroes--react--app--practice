cb801ded0fdc7e62a9001bfbcf3a8aa34c99f304
Erick Guzman <guzmi.dev@gmail.com>
2022-04-07 18:12:03 -0600

    :art: searchScreen finished

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -1,8 +1,70 @@
+import { useMemo } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../../selectors/getHeroByName";
+import HeroCard from "../hero/HeroCard";
+
 const SearchScreen = () => {
+  const navigate = useNavigate();
+  const [params] = useSearchParams();
+  const q = params.get("q") || "";
+
+  const [formValues, handleInputChange] = useForm({
+    searchText: q,
+  });
+
+  const { searchText } = formValues;
+  const heroesFilted = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`?q=${searchText}`);
+  };
+
   return (
-    <div>
-      <h1>SearchScreen</h1>
-    </div>
+    <>
+      <h1>Búsquedas</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr />
+
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              name="searchText"
+              id="searchText"
+              autoComplete="off"
+              className="form-control"
+              placeholder="Buscar un héroe"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button className="btn btn-outline-primary mt-1">Buscar...</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+          {q === "" ? (
+            <div className="alert alert-info">Buscar un heroe</div>
+          ) : (
+            heroesFilted.length === 0 && (
+              <div className="alert alert-danger">
+                No hay resultados de: {q}{" "}
+              </div>
+            )
+          )}
+
+          {heroesFilted.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/selectors/getHeroByName.js
@@ -0,0 +1,11 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByName = (name = "") => {
+  console.log("aaa");
+  if (name === "") {
+    return [];
+  }
+  name = name.toLowerCase();
+
+  return heroes.filter((hero) => hero.superhero.toLowerCase().includes(name));
+};
